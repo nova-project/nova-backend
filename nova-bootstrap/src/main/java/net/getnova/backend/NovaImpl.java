854ea657e52919dcb410bd0e074b198797442e02
Marcel Koch <koch_marcel@outlook.com>
2020-06-27 15:04:37 +0200

    Updated java runtime information log

--- nova-bootstrap/src/main/java/net/getnova/backend/NovaImpl.java
+++ nova-bootstrap/src/main/java/net/getnova/backend/NovaImpl.java
@@ -52,8 +52,7 @@ public final class NovaImpl implements Nova {
         log.info("| |\\  | (_) \\ V / (_| |  | |_) | (_| | (__|   <  __/ | | | (_| |");
         log.info("|_| \\_|\\___/ \\_/ \\__,_|  |____/ \\__,_|\\___|_|\\_\\___|_| |_|\\__,_|");
         final String version = NovaImpl.class.getPackage().getImplementationVersion();
-        log.info("Nova Version: " + (version == null ? "development" : version) + ", Java Runtime: "
-                + System.getProperty("java.vendor") + " v" + System.getProperty("java.version"));
+        log.info("Nova Version: " + (version == null ? "development" : version) + ", Java Runtime: " + System.getProperty("java.version"));
 
         NovaLogConfigurer.redirectSysLog();
 
