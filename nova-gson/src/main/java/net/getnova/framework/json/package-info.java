876d82eaa9323a1cbb518457fe23f130f623e801
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-03-10 13:51:51 +0100

    Removed module 'nova-gson'

--- nova-gson/src/main/java/net/getnova/framework/json/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package contains utils to serialize and deserialize java objects.
- */
-package net.getnova.framework.json;
--- nova-gson/src/main/java/net/getnova/framework/json/types/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * This package contains all special json type mappings.
- */
-package net.getnova.framework.json.types;
