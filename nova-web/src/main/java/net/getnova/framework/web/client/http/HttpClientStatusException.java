89b7ed4829eeadde81fd686a5c57bdfa02c08997
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-03-10 13:22:29 +0100

    Added http client

---
+++ nova-web/src/main/java/net/getnova/framework/web/client/http/HttpClientStatusException.java
@@ -0,0 +1,40 @@
+package net.getnova.framework.web.client.http;
+
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import net.getnova.framework.web.HttpStatusException;
+import reactor.netty.ByteBufMono;
+import reactor.netty.http.client.HttpClientResponse;
+
+@EqualsAndHashCode
+public class HttpClientStatusException extends HttpStatusException {
+
+  @Getter
+  private final HttpMethod method;
+  @Getter
+  private final String uri;
+  @Getter
+  private final HttpClientResponse response;
+  @Getter
+  private final ByteBufMono data;
+
+  public HttpClientStatusException(
+    final HttpMethod method,
+    final String uri,
+    final HttpClientResponse response,
+    final ByteBufMono data
+  ) {
+    super(response.status().toString() + " - " + method.toString() + " " + uri);
+    this.method = method;
+    this.uri = uri;
+    this.response = response;
+    this.data = data;
+  }
+
+  @Override
+  public HttpResponseStatus getStatus() {
+    return this.response.status();
+  }
+}
