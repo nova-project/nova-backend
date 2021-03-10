2e2d684f527034b3e1102c5e793fea2318d44672
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-03-10 13:48:07 +0100

    Removed module 'api-processor'

--- nova-api-processor/src/main/java/net/getnova/framework/api/processor/endpoint/EndpointProcessor.java
+++
@@ -1,8 +0,0 @@
-package net.getnova.framework.api.processor.endpoint;
-
-import javax.lang.model.element.ExecutableElement;
-
-public interface EndpointProcessor {
-
-  void process(ExecutableElement method);
-}
--- nova-api-processor/src/main/java/net/getnova/framework/api/processor/endpoint/GetEndpointProcessor.java
+++
@@ -1,5 +0,0 @@
-package net.getnova.framework.api.processor.endpoint;
-
-public class GetEndpointProcessor extends AbstractEndpointProcessor {
-
-}
