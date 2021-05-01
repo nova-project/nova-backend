be3c902a3aa6dabd2e1b967a79a37b11391d8abb
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-05-01 22:15:45 +0200

    Added JsonRawSerializer

---
+++ nova-core/src/main/java/net/getnova/framework/core/JsonRawSerializer.java
@@ -0,0 +1,18 @@
+package net.getnova.framework.core;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import java.io.IOException;
+
+public class JsonRawSerializer extends JsonSerializer<String> {
+
+  @Override
+  public void serialize(
+    final String value,
+    final JsonGenerator gen,
+    final SerializerProvider serializers
+  ) throws IOException {
+    gen.writeRawValue(value);
+  }
+}
